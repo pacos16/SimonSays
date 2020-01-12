a6845468ff537d402c40c0e799edc3a6615f53f0
Paco Signes <pacos16@gmail.com>
2020-01-12 20:54:26 +0100

    Machine play implemented

--- app/src/main/java/com/example/simonsays/GameFragment.java
+++ app/src/main/java/com/example/simonsays/GameFragment.java
@@ -7,6 +7,8 @@ import android.graphics.ColorFilter;
 import android.graphics.PixelFormat;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -21,6 +23,9 @@ import androidx.fragment.app.Fragment;
 
 import java.util.ArrayList;
 import java.util.Random;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class GameFragment extends Fragment {
     private AppCompatButton btGreen;
@@ -32,6 +37,10 @@ public class GameFragment extends Fragment {
     private Random random;
     private int turn;
     private Button btPlay;
+    private Handler handler;
+    private Executor executor;
+
+
 
     @Nullable
     @Override
@@ -42,6 +51,7 @@ public class GameFragment extends Fragment {
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+        turn=0;
         btPlay=getActivity().findViewById(R.id.btPlay);
         btGreen=getActivity().findViewById(R.id.btGreen);
         btBlue=getActivity().findViewById(R.id.btBlue);
@@ -50,12 +60,14 @@ public class GameFragment extends Fragment {
         tvScore=getActivity().findViewById(R.id.tvScore);
         tvScore.setBackgroundColor(Color.BLACK);
         tvScore.bringToFront();
+        tvScore.setText(String.valueOf(turn));
+        handler=new Handler();
 
         btGreen.setBackgroundTintList(ContextCompat.getColorStateList(getContext(),R.color.green_button));
         btYellow.setBackgroundTintList(ContextCompat.getColorStateList(getContext(),R.color.yellow_button));
         btRed.setBackgroundTintList(ContextCompat.getColorStateList(getContext(),R.color.red_button));
         btBlue.setBackgroundTintList(ContextCompat.getColorStateList(getContext(),R.color.blue_button));
-
+        executor= Executors.newSingleThreadExecutor();
 
         btPlay.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -64,6 +76,9 @@ public class GameFragment extends Fragment {
                 turn=1;
                 btPlay.setEnabled(false);
                 btPlay.setVisibility(View.GONE);
+                playSecuence=new ArrayList<>();
+                playSecuence.add(random.nextInt(4)+1);
+                play();
 
             }
         });
@@ -71,9 +86,130 @@ public class GameFragment extends Fragment {
 
     }
 
+    public class BeepAndStateSelected implements Runnable{
 
-    
+        private int position;
+        public BeepAndStateSelected(int position){
+            this.position=position;
+        }
+        @Override
+        public void run() {
+            switch (playSecuence.get(position)){
+                case 1:
+                    btGreen.setPressed(true);
+                    break;
+                case 2:
+                    btRed.setPressed(true);
+                    break;
+                case 3:
+                    btYellow.setPressed(true);
+                    break;
+                case 4:
+                    btBlue.setPressed(true);
+                    break;
+                default:
+                    Log.i("Cagadas","La has cagado con el random");
+            }
+            ResetColor resetColor=new ResetColor(position);
+            WaitAndReset waitAndReset=new WaitAndReset(resetColor);
+            executor.execute(waitAndReset);
+        }
+    }
 
+    public class WaitAndBeep implements Runnable{
+
+        BeepAndStateSelected beepAndStateSelected;
+
+        public WaitAndBeep(BeepAndStateSelected beepAndStateSelected) {
+            this.beepAndStateSelected = beepAndStateSelected;
+        }
+
+        @Override
+        public void run() {
+            try {
+                Thread.sleep(1000);
+                handler.post(beepAndStateSelected);
+            }catch (InterruptedException ie){
+
+            }
+        }
+    }
+
+    public class WaitAndReset implements Runnable{
+
+        ResetColor resetColor;
+
+        public WaitAndReset(ResetColor resetColor) {
+            this.resetColor = resetColor;
+        }
+
+        @Override
+        public void run() {
+            try {
+                Thread.sleep(300);
+                handler.post(resetColor);
+            }catch (InterruptedException ie){
+
+            }
+
+        }
+    }
+
+    public class ResetColor implements Runnable{
+        private int position;
+
+        public ResetColor(int position) {
+            this.position = position;
+        }
+
+        @Override
+        public void run() {
+            switch (playSecuence.get(position)){
+                case 1:
+                    btGreen.setPressed(false);
+                    break;
+                case 2:
+                    btRed.setPressed(false);
+                    break;
+                case 3:
+                    btYellow.setPressed(false);
+                    break;
+                case 4:
+                    btBlue.setPressed(false);
+                    break;
+                    default:
+                        Log.i("Cagadas","La has cagado con el random");
+            }
+            if(position<playSecuence.size()-1){
+                BeepAndStateSelected beepAndStateSelected=new BeepAndStateSelected(position+1);
+                WaitAndBeep waitAndBeep=new WaitAndBeep(beepAndStateSelected);
+                executor.execute(waitAndBeep);
+            }else{
+                playSecuence.add(random.nextInt(4)+1);
+            }
+        }
+    }
+
+
+    private void play(){
+
+        BeepAndStateSelected beepAndStateSelected=new BeepAndStateSelected(0);
+        WaitAndBeep waitAndBeep=new WaitAndBeep(beepAndStateSelected);
+        executor.execute(waitAndBeep);
+
+    }
+
+    public void jugadorPlay(){
+
+    }
+
+    public void disableButtons(){
+
+    }
+
+    public void enableButtons(){
+
+    }
 
 
 
